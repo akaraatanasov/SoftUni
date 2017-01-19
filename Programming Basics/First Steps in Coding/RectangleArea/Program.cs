fbc2022600f3db1fb18435db89a6eb27e538344b
akaraatanasov <akaraatanasov@gmail.com>
2017-01-19 02:01:39 +0200

    Added First Steps in Coding Directory

---
+++ Programming Basics/First Steps in Coding/RectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type side A of the rectangle:");
+            var a = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Type side B of the rectangle:");
+            var b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("The area of the rectangle is:");
+            Console.WriteLine(a * b);
+        }
+    }
+}
---
+++ Programming Basics/First Steps in Coding/SquareOfStars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', num));
+            for (int i = 0; i < num - 2; i++)
+            {
+                Console.WriteLine("*{0}*", new string(' ', num - 2));
+            }
+            Console.WriteLine(new string('*', num));
+        }
+    }
+}
