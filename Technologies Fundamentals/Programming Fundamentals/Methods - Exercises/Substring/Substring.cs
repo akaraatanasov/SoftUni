0425f048c814709737e27d55d5aaa974c8184450
akaraatanasov <akaraatanasov@gmail.com>
2017-01-25 05:55:06 +0200

    Added rest of Methods - Exercises

---
+++ Technologies Fundamentals/Programming Fundamentals/Methods - Exercises/Substring/Substring.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Substring
+{
+    class Substring
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine()) + 1;
+
+            char Search = 'p';
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    int endIndex = jump;
+
+                    if (endIndex + i > text.Length)
+                    {
+                        endIndex = text.Length - i;
+                    }
+
+                    string matchedString = text.Substring(i, endIndex);
+                    Console.WriteLine(matchedString);
+
+                    i += (endIndex - 1);
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
