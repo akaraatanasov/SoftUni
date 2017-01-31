e955e17bdbba907930555b001ec0cc78657d7183
akaraatanasov <akaraatanasov@gmail.com>
2017-02-01 00:25:43 +0200

    Changed SequenceOfCmd project

--- Technologies Fundamentals/Programming Fundamentals/Methods - Exercises/SequenceOfCmd/Sequence.cs
+++ Technologies Fundamentals/Programming Fundamentals/Methods - Exercises/SequenceOfCmd/Sequence.cs
@@ -11,42 +11,37 @@ namespace SequenceOfCmd
         {
             int sizeOfArray = int.Parse(Console.ReadLine());
 
-            long[] array = Console.ReadLine()
+            int[] array = Console.ReadLine()
                 .Split(ArgumentsDelimiter)
-                .Select(long.Parse)
+                .Select(int.Parse)
                 .ToArray();
 
             string command = Console.ReadLine();
 
-            while (!command.Equals("over"))
+            while (!command.Equals("stop"))
             {
-                string line = Console.ReadLine().Trim();
+                string[] line = command.Split(ArgumentsDelimiter).ToArray();
                 int[] args = new int[2];
 
-                if (command.Equals("add") ||
-                    command.Equals("substract") ||
-                    command.Equals("multiply"))
+                if (line[0].Equals("add") || line[0].Equals("subtract") || line[0].Equals("multiply"))
                 {
-                    string[] stringParams = line.Split(ArgumentsDelimiter);
-                    args[0] = int.Parse(stringParams[0]);
-                    args[1] = int.Parse(stringParams[1]);
-
-                    PerformAction(array, command, args);
+                    args[0] = int.Parse(line[1]);
+                    args[1] = int.Parse(line[2]);
+                    PerformAction(array, line[0], args);
+                    Console.WriteLine(String.Join(" ", array));
                 }
-
-                PerformAction(array, command, args);
-
-                PrintArray(array);
-                Console.WriteLine('\n');
+                else
+                {
+                    PerformAction(array, line[0]);
+                }
 
                 command = Console.ReadLine();
             }
         }
 
-        static void PerformAction(long[] arr, string action, int[] args)
+        public static void PerformAction(int[] array, string action, int[] args)
         {
-            long[] array = arr.Clone() as long[];
-            int pos = args[0];
+            int pos = args[0] - 1;
             int value = args[1];
 
             switch (action)
@@ -60,37 +55,46 @@ namespace SequenceOfCmd
                 case "subtract":
                     array[pos] -= value;
                     break;
-                case "lshift":
-                    ArrayShiftLeft(array);
-                    break;
-                case "rshift":
-                    ArrayShiftRight(array);
-                    break;
             }
         }
 
-        private static void ArrayShiftRight(long[] array)
+        public static void PerformAction(int[] array, string action)
         {
-            for (int i = array.Length - 1; i >= 1; i--)
+            if (action.Equals("lshift"))
             {
-                array[i] = array[i - 1];
+                ArrayShiftLeft(array);
+            }
+            if (action.Equals("rshift"))
+            {
+                ArrayShiftRight(array);
             }
         }
 
-        private static void ArrayShiftLeft(long[] array)
+        private static void ArrayShiftRight(int[] array)
         {
+            int[] rotatedArray = new int[array.Length];
+
+            rotatedArray[0] = array[array.Length - 1];
+            for (int i = 1; i < array.Length; i++)
+            {
+                rotatedArray[i] = array[i - 1];
+            }
+
+            Console.WriteLine(String.Join(" ", rotatedArray));
+        }
+
+        private static void ArrayShiftLeft(int[] array)
+        {
+            int[] rotatedArray = new int[array.Length];
+
+            rotatedArray[array.Length - 1] = array[0];
             for (int i = 0; i < array.Length - 1; i++)
             {
-                array[i] = array[i + 1];
+                rotatedArray[i] = array[i+1];
             }
+
+            Console.WriteLine(String.Join(" ", rotatedArray));
         }
 
-        private static void PrintArray(long[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i] + " ");
-            }
-        }
     }
 }
