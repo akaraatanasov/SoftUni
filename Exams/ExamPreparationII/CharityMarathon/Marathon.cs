3c604d391c5162abfbb31a43d385e720536eb33f
akaraatanasov <akaraatanasov@gmail.com>
2017-02-22 01:40:01 +0200

    Added ExamPreparationII

---
+++ Exams/ExamPreparationII/CharityMarathon/Marathon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CharityMarathon
+{
+    public class Marathon
+    {
+        public static void Main()
+        {
+            uint days = uint.Parse(Console.ReadLine());
+            uint runnersNum = uint.Parse(Console.ReadLine());
+            uint avgLaps = uint.Parse(Console.ReadLine());
+            uint trackLength = uint.Parse(Console.ReadLine());
+            uint trackCapacity = uint.Parse(Console.ReadLine());
+            double moneyPK = double.Parse(Console.ReadLine());
+
+            uint runnersMax = days * trackCapacity;
+            if (runnersNum > runnersMax)
+            {
+                runnersNum = runnersMax;
+            }
+            long totalKM = (long)runnersNum * avgLaps * trackLength / 1000;
+            double result = totalKM * moneyPK;
+
+            Console.WriteLine($"Money raised: {result:f2}");
+        }
+    }
+}
---
+++ Exams/ExamPreparationII/Ladybugs/Ladybugs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace Ladybugs
+{
+    public class Ladybugs
+    {
+        public static void Main()
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] bugField = Enumerable.Repeat(0, fieldSize).ToArray();
+
+            var bugIndexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int size = Math.Min(fieldSize, bugIndexes.Count());
+            foreach (var index in bugIndexes)
+            {
+                if (index >= 0 && index < fieldSize)
+                {
+                    bugField[index] = 1;
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (!command.Equals("end"))
+            {
+                var commandParameters = command.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                int currBugIndex = int.Parse(commandParameters[0]);
+                string currDirection = commandParameters[1];
+                int flyLength = int.Parse(commandParameters[2]);
+
+                if (currBugIndex >= 0 && currBugIndex < fieldSize && bugField[currBugIndex] == 1)
+                {
+                    bugField[currBugIndex] = 0;
+                    if (currDirection.Equals("right"))
+                    {
+                        if (currBugIndex + flyLength >= 0 && currBugIndex + flyLength < fieldSize)
+                        {
+                            while (bugField[currBugIndex + flyLength] == 1 && currBugIndex + flyLength < fieldSize - 1)
+                            {
+                                flyLength++;
+                            }
+                            bugField[currBugIndex + flyLength] = 1;
+                        }
+                    }
+                    else if (currDirection.Equals("left"))
+                    {
+                        if (currBugIndex - flyLength >= 0 && currBugIndex - flyLength < fieldSize)
+                        {
+                            while (bugField[currBugIndex - flyLength] == 1 && currBugIndex - flyLength > 0)
+                            {
+                                flyLength--;
+                            }
+                            bugField[currBugIndex - flyLength] = 1;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", bugField));
+
+        }
+    }
+}
